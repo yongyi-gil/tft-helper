118941fab8fb7a662c2c2ad4dfa5afe799473700
yongyi-gil <yongyi1587@gmail.com>
2023-12-20 12:55:07 +0900

    fix: 라우팅 수정

--- src/component/header/Header.tsx
+++ src/component/header/Header.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
 export default function Header() {
   return (
     <div className="header">
-      <h1>TFT Season.10 Helper</h1>
+      <Link
+        href="/"
+      >
+        <h1>TFT Season.10 Helper</h1>
+      </Link>
     </div>
   )
 }
