5ba349fbd35d5ac83873be246d4a49d8fb25e09e
yongyi-gil <yongyi1587@gmail.com>
2023-12-22 12:23:01 +0900

    fix: summoner.d.ts Summoner 타입 추가

---
+++ src/types/summoner.d.ts
@@ -0,0 +1,16 @@
+export type SummnerType = {
+  "puuid": string;
+  "leagueId": string;
+  "queueType": string;
+  "tier": string;
+  "rank": string;
+  "summonerId": string;
+  "summonerName": string;
+  "leaguePoints": number;
+  "wins": number;
+  "losses": number;
+  "veteran": boolean;
+  "inactive": boolean;
+  "freshBlood": boolean;
+  "hotStreak": boolean;
+}
